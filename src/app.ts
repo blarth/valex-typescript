5e2d0749c242edbcd6151ee6dc94f15e47624937
blarth <jminocente@gmail.com>
2022-04-14 13:49:39 -0300

    fix: imports

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ import router from "./routers/index.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use((error : Error, req : Request, res : Response, next : NextFunction) => {
-  /* if (error.type === "invalid_entity") return res.sendStatus(422);
-  if (error.type === "auth_error") return res.sendStatus(401); */
+app.use((error , req : Request, res : Response, next : NextFunction) => {
+  if (error.type === "invalid_entity") return res.sendStatus(422);
+  if (error.type === "auth_error") return res.sendStatus(401);
   return res.sendStatus(500);
 })
 app.use(router)
