d22b771b66dd527501b1cd2d34cd47f4a9a5705c
Celso <celsolnv27@gmail.com>
2022-04-19 23:48:17 -0300

    Preparando revisão de código

--- src/controllers/onlinePaymentsController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import * as onlinePaymentService from "../services/onlinePaymentService.js"
-
-export async function postOnlinePayment(req: Request, res: Response){
-    const {idBusiness} = req.params
-    const {cardNumber, cardholderName, expirationDate, securityCode, amount} = req.body
-    await onlinePaymentService.onlinePayment(cardNumber, cardholderName, expirationDate, securityCode, Number(idBusiness), amount)
-    res.sendStatus(200)
-}
